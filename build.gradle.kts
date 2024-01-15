52c7612e7acdb0112b13423944530707ae5da585
Rakib <hasan.rakib@datasoft-bd.com>
2024-01-15 15:44:54 +0600

    added static code analysis stage.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.sonarqube") version "4.4.1.3373"
 }
-sonarqube {
-    properties {
-        property("sonar.sources", "./src/main")
-        property("sonar.host.url", "https://sonarcloud.io/")
-        property("sonar.organization", "rakibhasan1030")
-        property("sonar.projectKey", "rakibhasan1030_TipCalculator")
-        property("sonar.projectName", "TipCalculator")
-        property("sonar.gradle.skipCompile", "true")
-    }
-}
