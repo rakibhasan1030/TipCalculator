7248e6f89a9d05f717de9ef827588f0ababcde41
Rakib <hasan.rakib@datasoft-bd.com>
2024-01-18 11:56:59 +0600

    added static code analysis stage.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("org.sonarqube") version "4.4.1.3373"
+    //id("org.sonarqube") version "4.4.1.3373"
 }
